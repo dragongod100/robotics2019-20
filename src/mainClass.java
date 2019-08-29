e5846502219f7b37632d489463c108ae30c05e6d
dragon <draogngod100.snedhlep@gmail.com>
2019-08-29 11:01:27 -0500

    Nothing

--- src/mainClass.java
+++ src/mainClass.java
@@ -4,45 +4,20 @@ public class mainClass
 {
 	public static void main(String args[])
 	{
-		towerCubes Cube = new towerCubes();
-		Scanner input = new Scanner(System.in);
-		String winAuton;
-		int autonPoints;
-		switch(input.next(winAuton))
-		{
-			case "Yes":
-				autonPoints = 6;
-				break;
-			case "No":
-				autonPoints = 0;
-				break;
-			default:
-				autonPoints = 3;
-				break;
-		}
-		print("Green Scored");
-		Cube.Green.scored = input.nextInt();
-		print("Purple Scored");
-		Cube.Purple.scored = input.nextInt();
-		print("Orange Scored");
-		Cube.Orange.scored = input.nextInt();
 
-		print("Stacked Green");
-		Cube.Green.currentVal = multiplier(Cube.Green.scored) * input.nextInt();
-		print("Stacked Purple");
-		Cube.Purple.currentVal = multiplier(Cube.Purple.scored) * input.nextInt();
-		print("Stacked Orange");
-		Cube.Orange.currentVal = multiplier(Cube.Orange.scored) * input.nextInt();
-		
-		print(String.valueOf(Cube.Green.currentVal));
-		print(String.valueOf(Cube.Purple.currentVal));
-		print(String.valueOf(Cube.Orange.currentVal));
-		print("Your score is: " + String.valueOf(Cube.Green.currentVal + Cube.Orange.currentVal + Cube.Purple.currentVal));
+
+		towerCubes Cube = new towerCubes();
+		Scanner f = new Scanner(System.in);
+		runtime(f, Cube);
+		runtime(f, Cube);
+		f.close();
 	}
+
 	private static void print(String out)
 	{
 		System.out.println(out);
 	}
+
 	private static int multiplier(int input)
 	{
 		if(input == 0)
@@ -53,5 +28,54 @@ public class mainClass
 		{
 			return input + 1;
 		}
+	}
+	public static void Scored(Scanner name, towerCubes Cube)
+	{
+		print("Green Scored");
+		Cube.Green.scored = name.nextInt();
+		print("Purple Scored");
+		Cube.Purple.scored = name.nextInt();
+		print("Orange Scored");
+		Cube.Orange.scored = name.nextInt();
+	}
+	public static void Stacked(Scanner name, towerCubes Cube)
+	{
+		print("Stacked Green");
+		Cube.Green.currentVal = multiplier(Cube.Green.scored) * name.nextInt();
+		print("Stacked Purple");
+		Cube.Purple.currentVal = multiplier(Cube.Purple.scored) * name.nextInt();
+		print("Stacked Orange");
+		Cube.Orange.currentVal = multiplier(Cube.Orange.scored) * name.nextInt();
+	}
+
+	public static void Scoring(towerCubes Cube, int autonPoints)
+	{
+		print("The score is: " + String.valueOf(autonPoints +
+				Cube.Green.currentVal +
+				Cube.Orange.currentVal +
+				Cube.Purple.currentVal));
+	}
+
+	public static void runtime(Scanner name, towerCubes Cube)
+	{
+		print("Auton Win");
+		int autonPoints;
+		switch(name.next())
+		{
+			case "Yes":
+				autonPoints = 6;
+				break;
+			case "Tie":
+				autonPoints = 3;
+				break;
+			default:
+				autonPoints = 0;
+				break;
+		}
+		Scored(name, Cube);
+		Stacked(name, Cube);
+		Scoring(name, Cube, autonPoints);
+
+
 	}
 }
